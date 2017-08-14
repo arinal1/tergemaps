4275d382f85f12d73a7de29554ac6cd8ca4dc0de
arinal1 <rinal@email.com>
2017-08-14 08:53:34 +0700

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,41 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Storage } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation';
+import { AgmCoreModule } from '@agm/core'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FirstPage } from '../pages/first/first';
+import { PlacesService } from '../services/places.service';
+import { TempatPage } from '../pages/tempat/tempat';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FirstPage,
+    TempatPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBQL8r5lzVLYbt5aa8Fvblo6ZIMho4M9hw'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FirstPage,
+    TempatPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, PlacesService, Storage, Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { ModalController, NavController } from 'ionic-angular';
+import { FirstPage } from '../first/first';
+import { TempatPage } from '../tempat/tempat';
+import { PlacesService } from '../../services/places.service';
+import { Place } from '../../model/place.model';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  places: {title: string}[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+   public navCtrl: NavController,
+   private placesService: PlacesService,
+   private modalCtrl: ModalController) {
 
   }
 
+  ionViewWillEnter() {
+  	this.placesService.getPlaces()
+  		.then(
+  			(places) => this.places = places 
+  		);
+  }
+
+  onLoadFirstPage() {
+  	this.navCtrl.push(FirstPage)
+  }
+
+  onOpenPlace(place: Place) {
+  	this.modalCtrl.create(TempatPage, place).present();
+  }
 }
